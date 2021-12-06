ab53667c43da957a26a55b843feb2e8a8d2a88d0
cheddarbunny <bunny@bunny.com>
2021-12-05 19:12:34 -0500

    Add project files.

---
+++ Asign 15b/Source.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+//Template
+template <class T>
+//Function half
+T half(T value) 
+{
+
+}
+
+//Main Function
+int main()
+{
+    //Tests
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+}
